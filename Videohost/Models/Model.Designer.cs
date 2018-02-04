58e1070d9e0ac1c9296bab8d6d47b2d2e4247c16
Vladislav <vlad_24_9898@mail.ru>
2018-02-04 20:21:54 +0300

    Мелкий рефакторинг

--- Videohost/Models/Model.Designer.cs
+++ Videohost/Models/Model.Designer.cs
@@ -1,4 +1,4 @@
-﻿// Создание кода T4 для модели "C:\Users\Vladislav\Documents\Visual Studio 2017\Projects\Videohost\Videohost\Models\Model.edmx" включено. 
+﻿// Создание кода T4 для модели "C:\Users\Vladislav\Desktop\Videohost\Videohost\Models\Model.edmx" включено. 
 // Чтобы включить формирование кода прежних версий, измените значение свойства "Стратегия создания кода" конструктора
 // на "Legacy ObjectContext". Это свойство доступно в окне "Свойства", если модель
 // открыта в конструкторе.
